6c54b88e162aa4cdf4cce66fac6cc0984da5c61c
FrantzCFelix <Frantz.C.Felix@gmail.com>
2020-05-23 07:36:59 -0700

    updated controller

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -4,42 +4,40 @@ const express = require('express');
 
 const router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-const cat = require('../models/cat.js');
 
-// Create all our routes and set up logic within those routes where required.
+const burger = require('../models/burger.js');
+
+
 router.get('/', (req, res) => {
-  cat.all(function(data) {
+  burger.all(function(data) {
     const hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render('index', hbsObject);
   });
 });
 
-router.post('/api/cats', (req, res) => {
-  cat.create(['name', 'sleepy'], [req.body.name, req.body.sleepy], function(
-    result
-  ) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+router.post('/api/burgers', (req, res) => {
+  burger.create(req.body.burger_name, function(result) {
+    console.log(result);
+    res.redirect("/");
   });
 });
 
-router.put('/api/cats/:id', (req, res) => {
+router.put('/api/burgers/:id', (req, res) => {
   const condition = 'id = ' + req.params.id;
 
   console.log('condition', condition);
 
-  cat.update(
+  burger.update(
     {
-      sleepy: req.body.sleepy
+      devoured: req.body.devoured
     },
     condition,
     function(result) {
       if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
+        
         return res.status(404).end();
       } else {
         res.status(200).end();
@@ -48,12 +46,12 @@ router.put('/api/cats/:id', (req, res) => {
   );
 });
 
-router.delete('/api/cats/:id', (req, res) => {
+router.delete('/api/burgers/:id', (req, res) => {
   const condition = 'id = ' + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
+      
       return res.status(404).end();
     } else {
       res.status(200).end();
